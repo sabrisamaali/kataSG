627b02a96a6a16b0015f71a1ee3e5efeab0f5a2e
sabrisamaali <sabri.smli@gmail.com>
2018-06-24 00:04:09 +0200

    second commit

--- kata/src/main/java/com/logware/kata/StartKata.java
+++ kata/src/main/java/com/logware/kata/StartKata.java
@@ -3,14 +3,37 @@ package com.logware.kata;
 import com.logware.kata.entities.Account;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StartKata {
 
     public static void main(String[] args) {
         Account account = new Account(0, new ArrayList<>());
-        account.saveMoney(50);
-        account.retrieveMoney(25);
-        System.out.println("you have : " + account.getAmount() + "in your bank account");
-        account.showHistory();
+        while (true) {
+            System.out.println("please choose a number: < 1 > Deposit money | < 2 > Withdrawal < 3 > Show history  :");
+            Scanner choiceInput = new Scanner(System.in);
+            Scanner amountInout = new Scanner(System.in);
+            String operation = choiceInput.nextLine();
+            switch (operation) {
+                case "1": {
+
+                    System.out.println("please enter the amount : ");
+                    String amount = amountInout.nextLine();
+                    account.saveMoney(Double.parseDouble(amount));
+                    break;
+                }
+                case "2": {
+                    System.out.println("please enter the amount : ");
+                    String amount = amountInout.nextLine();
+                    account.retrieveMoney(Double.parseDouble(amount));
+                    break;
+                }
+                case "3": {
+                    account.showHistory();
+                }
+                default:
+                    break;
+            }
+        }
     }
 }
